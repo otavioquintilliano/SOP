32f03fcd7d409a2f9503af11011bb4a26af5a66c
otavioquintilliano <159180588+otavioquintilliano@users.noreply.github.com>
2024-03-14 12:54:12 -0300

    Add files via upload

---
+++ Agendador.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+@echo.
+@echo       *********************************
+@echo       ***                           ***
+@echo       ***    Hora do almoco : .BAT  ***
+@echo       ***                           ***   
+@echo       *********************************
+@echo.
+pause
